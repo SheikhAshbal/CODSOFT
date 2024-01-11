b047410ddbd63216dd4ef5add4facbed5a8f90f1
SheikhAshbal <ashbalfaheem@gmail.com>
2024-01-11 16:01:39 +0500

    Initialize

---
+++ Task_2(Calculater)/Calculater.py
@@ -0,0 +1,68 @@
+def options():
+    
+    print("""Below are the Options to perform Basic Arithmetic Operations         
+        1. Addition
+        2. Multiplication
+        3. Subtraction
+        4. Division 
+        For Exit press 0""")
+    global operation
+    operation=input("Enter the operation number(1/2/3/4) ")
+def addition():
+    add_accumulater=0
+    numbers=int(input("Enter the numbers you want to add:"))
+    for i in range(1,numbers+1):
+        num=float(input(f"Enter the num{i}: "))
+        add_accumulater+=num
+    return numbers,add_accumulater
+def subtraction():
+    num1=float(input("Enter the value of num1:"))
+    num2=float(input("Enter the value of num2:"))
+    subtract=num1-num2
+    return subtract
+def multiplication():
+    product=1
+    numbers=int(input("Enter the numbers you want to product:"))
+    for i in range(1,numbers+1):
+        num=float(input(f"Enter the num {i}: "))
+        product*=num
+    return numbers,product    
+def division():
+    num1=float(input("Enter the value of num1:"))
+    num2=float(input("Enter the value of num2:"))
+    while True:
+        try:
+            divide=num1/num2
+            return divide
+        except:
+            print("Error!!!, cannot divide by zero")
+            num2=float(input("Enter the value of num2 again:")) 
+def main():
+    
+    options()
+        
+    if operation=="1":
+        result=addition()
+        print(f"The addition of {result[0]} numbers is:{result[1]}")
+               
+    elif operation=="2":
+        result=multiplication()
+        print(f"The product of {result[0]} numbers is :{result[1]}")
+                
+    elif operation=="3":
+        print(f"The result is:{subtraction()}")
+                
+    elif operation=="4":
+        print(f"The result is:{division()}")
+                
+    else:
+        print("Enter a valid input!")
+                
+main()
+permission=input("To continue the calculater press Y and press N for stop:").upper()
+while permission=="Y":
+    main()
+    permission=input("To continue the calculater press Y and press N for stop:")
+if permission=="N":
+    print("DONE!!")
+   
---
+++ Task_3(Password_Generater)/Password_Generater.py
@@ -0,0 +1,48 @@
+import random
+import string
+import pyperclip
+def main(): 
+    print("------------>PASSWORD GENERATOR<------------\n")
+    password_generator_menu()
+def generate_password(length):
+    characters = string.ascii_letters + string.digits 
+    password = ''.join(random.choice(characters) for _ in range(length))
+    return password
+def Password_length():
+    while True:
+        try:
+            global length
+            length= int(input("Enter the desired length of the password: "))
+            if length <= 0:
+                raise ValueError("Length must be a positive integer.")
+            break
+        except ValueError as e:
+            print(f"Error: {e}")
+    return length
+def password_generator_menu():
+    password = generate_password(Password_length())
+    print(f"\nGenerated Password: {password}\n")
+    while True:
+            user_ask=input("""Choose an option(1/2/3/4)---->
+                       1.Regenerate Password
+                       2.Change Length then Generate
+                       3.Copy the Password
+                       4.Exit  :""")
+            if user_ask=="1":
+                password = generate_password(length)
+                print(f"\nNew Generated Password: {password}\n")
+            elif user_ask=="2":
+                New_length=Password_length()
+                password = generate_password(New_length)
+                print(f"\nNew Generated Password: {password}")
+            elif user_ask=="3":
+                pyperclip.copy(password)
+                print("\nPassword copied to clipboard.\n")
+            elif user_ask=="4":
+                print("\nExiting...")
+                break
+            else:
+                print("Invalid Input")
+
+if __name__ == "__main__":
+    main()
